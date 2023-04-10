f7fc0505685c80effb340177b39950c6d4bf34c1
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-04-10 13:00:23 +0600

    apply now button added on job details page

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -56,55 +56,63 @@ const JobDetails = () => {
             <p className="text-base font-medium text-gray">{experiences}</p>
           </div>
         </div>
-        <div className="p-8 bg-violet-200 rounded-lg">
-          <p className="text-xl font-extrabold mb-6">Job Details</p>
-          <hr className="text-violet-300 mb-6" />
-          <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-4">
-            <FontAwesomeIcon
-              className="text-blueGradient"
-              icon={faMoneyBillAlt}
-            />
-            <p className="font-bold">
-              <span className="text-darkGray font-extrabold">Salary</span> :{" "}
-              {salary} (per month)
-            </p>
+        <div className="mb-32">
+          <div className="bg-violet-200 p-8 rounded-lg mb-6">
+            <p className="text-xl font-extrabold mb-6">Job Details</p>
+            <hr className="text-violet-300 mb-6" />
+            <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-4">
+              <FontAwesomeIcon
+                className="text-blueGradient"
+                icon={faMoneyBillAlt}
+              />
+              <p className="font-bold">
+                <span className="text-darkGray font-extrabold">Salary</span> :{" "}
+                {salary} (per month)
+              </p>
+            </div>
+            <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-8">
+              <FontAwesomeIcon
+                className="text-blueGradient"
+                icon={faCalendarAlt}
+              />
+              <p className="font-bold">
+                <span className="text-darkGray font-extrabold">Job Title</span>{" "}
+                : {job_title}
+              </p>
+            </div>
+            <p className="text-xl font-extrabold mb-6">Contact Information</p>
+            <hr className="text-violet-300 mb-6" />
+            <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-4">
+              <FontAwesomeIcon className="text-blueGradient" icon={faPhone} />
+              <p className="font-bold">
+                <span className="text-darkGray font-extrabold">Phone</span> :{" "}
+                {contact_information?.phone}
+              </p>
+            </div>
+            <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-4">
+              <FontAwesomeIcon
+                className="text-blueGradient"
+                icon={faEnvelope}
+              />
+              <p className="font-bold">
+                <span className="text-darkGray font-extrabold">Email</span> :{" "}
+                {contact_information?.email}
+              </p>
+            </div>
+            <div className="flex gap-2 text-gray text-lg font-semibold items-baseline">
+              <FontAwesomeIcon
+                className="text-blueGradient"
+                icon={faLocationDot}
+              />
+              <p className="font-bold">
+                <span className="text-darkGray font-extrabold">Address</span> :{" "}
+                {location}
+              </p>
+            </div>
           </div>
-          <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-8">
-            <FontAwesomeIcon
-              className="text-blueGradient"
-              icon={faCalendarAlt}
-            />
-            <p className="font-bold">
-              <span className="text-darkGray font-extrabold">Job Title</span> :{" "}
-              {job_title}
-            </p>
-          </div>
-          <p className="text-xl font-extrabold mb-6">Contact Information</p>
-          <hr className="text-violet-300 mb-6" />
-          <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-4">
-            <FontAwesomeIcon className="text-blueGradient" icon={faPhone} />
-            <p className="font-bold">
-              <span className="text-darkGray font-extrabold">Phone</span> :{" "}
-              {contact_information?.phone}
-            </p>
-          </div>
-          <div className="flex gap-2 text-gray text-lg font-semibold items-baseline mb-4">
-            <FontAwesomeIcon className="text-blueGradient" icon={faEnvelope} />
-            <p className="font-bold">
-              <span className="text-darkGray font-extrabold">Email</span> :{" "}
-              {contact_information?.email}
-            </p>
-          </div>
-          <div className="flex gap-2 text-gray text-lg font-semibold items-baseline">
-            <FontAwesomeIcon
-              className="text-blueGradient"
-              icon={faLocationDot}
-            />
-            <p className="font-bold">
-              <span className="text-darkGray font-extrabold">Address</span> :{" "}
-              {location}
-            </p>
-          </div>
+          <button className="bg-blueGradient w-full text-white border-none py-4 rounded-lg text-xl font-extrabold">
+            Apply Now
+          </button>
         </div>
       </div>
     </div>
